dacca6e4cb93550086e7ad72dcbeb36eca7484fa
rickygv99 <rickygv99@yahoo.com>
2019-07-09 13:38:34 -0700

    Fixed syntax errors

--- server/routes/coins.js
+++ server/routes/coins.js
@@ -2,15 +2,15 @@ var express = require('express');
 var router = express.Router();
 var fetch = require('node-fetch');
 
-const coinMarketCapURL = "https://pro-api.coinmarketcap.com/v1";
-const coinMarketCapAPI_KEY = "9dd656b1-9f39-4166-a045-7f1d593708b0";
+const COIN_MARKET_CAP_URL = "https://pro-api.coinmarketcap.com/v1";
+const COIN_MARKET_CAP_API_KEY = "9dd656b1-9f39-4166-a045-7f1d593708b0";
 
 router.get('/coin', (req, res) => {
-  fetch(coinMarketCAPURL + "/cryptocurrency/info?slug=" + req.query.slug, {
+  fetch(COIN_MARKET_CAP_URL + "/cryptocurrency/info?slug=" + req.query.slug, {
     method: 'GET',
     mode: 'cors',
     headers: {
-      "X-CMC_PRO_API_KEY": coinMarketCapAPI_KEY
+      "X-CMC_PRO_API_KEY": COIN_MARKET_CAP_API_KEY
     }
   }).then(response => response.json())
     .then(response => {

e6cb704e0bed4476d1c41bad6b3c564151f604ec
rickygv99 <rickygv99@yahoo.com>
2019-07-09 11:35:17 -0700

    Added queries for symbol, description, and websites of a cryptocurrency

--- server/routes/coins.js
+++ server/routes/coins.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var fetch = require('node-fetch');
+
+const coinMarketCapURL = "https://pro-api.coinmarketcap.com/v1";
+const coinMarketCapAPI_KEY = "9dd656b1-9f39-4166-a045-7f1d593708b0";
 
 router.get('/coin', (req, res) => {
-
+  fetch(coinMarketCAPURL + "/cryptocurrency/info?slug=" + req.query.slug, {
+    method: 'GET',
+    mode: 'cors',
+    headers: {
+      "X-CMC_PRO_API_KEY": coinMarketCapAPI_KEY
+    }
+  }).then(response => response.json())
+    .then(response => {
+    return response.data[req.query.slug];
+  }).catch(err => {
+    console.error(err);
+  });
 })
 
 module.exports = router;
---
+++ thingpedia/device.js
@@ -0,0 +1,67 @@
+"use strict";
+
+const Tp = require('thingpedia');
+
+let options = {
+  dataContentType: "application/json"
+};
+
+module.exports = class Cryptocurrency extends Tp.BaseDevice {
+  constructor(engine, state) {
+    super(engine, state);
+
+    this.name = "Cryptocurrency Account for " + this.state.username;
+    this.description = "This is your Cryptocurrency Account. You can use it"
+      + " to do anything and everything related to cryptocurrencies.";
+
+    /* Stores the patient's credentials in our database */
+    let data = JSON.stringify({
+      username: this.state.username,
+      password: this.state.password
+    });
+
+    Tp.Helpers.Http.post("http://127.0.0.1:3000/signup", data, options)
+    .catch(err => {
+      console.error(err);
+    });
+  }
+
+  /*
+   * Returns the cryptocurrency's symbol
+   */
+  get_symbol({ slug }) {
+    Tp.Helpers.Http.get("http://127.0.0.1:3000/coin?slug=" + slug, options)
+    .then(response => {
+      return response.symbol;
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  }
+
+  /*
+   * Returns a description of the cryptocurrency
+   */
+  get_description({ slug }) {
+    Tp.Helpers.Http.get("http://127.0.0.1:3000/coin?slug=" + slug, options)
+    .then(response => {
+      return response.description;
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  }
+
+  /*
+   * Returns the websites for the cryptocurrency
+   */
+  get_websites({ slug }) {
+    Tp.Helpers.Http.get("http://127.0.0.1:3000/coin?slug=" + slug, options)
+    .then(response => {
+      return response.urls.website;
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  }
+};
